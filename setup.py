d99aebcf46f5dabf13a66d93ef6591f63bc5b137
ahriben gonzalez <ahribeng@gmail.com>
2019-08-22 17:10:20 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'apc_pdu_controller',
+  packages = ['apc_pdu_controller'],
+  version = '0.1',
+  license='MIT',
+  description = 'SSH Control for APC Model PDU's',
+  author = 'Ahriben Gonzalez',
+  author_email = 'ahribeng@gmail.com',
+  url = 'https://github.com/user/ahribeng',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['SSH', 'APC', 'PDU'],
+  install_requires=[
+          'paramiko'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+      ],
+)
